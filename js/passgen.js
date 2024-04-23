f8d0c816efe733da9d8b0e611726741c7217b5fd
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-04-23 14:30:01 +0200

    [UPD] Last name reading also added

--- js/passgen.js
+++ js/passgen.js
@@ -3,3 +3,8 @@
 
 let firstName = prompt("Inserisci il tuo nome:"); /* INSERIMENTO NOME AL INTERNO DELLA VARIABILE FISTNAME */
 console.log("L'utente ha inserito " + (firstName)); /* LOG DEL CONTENUTO DELLA VARIABILE FIRSTNAME */
+
+/* STESSA LETTURA DI SOPRA */
+let surName = prompt("Inserici il tuo congonome:");
+console.log("L'utente ha inserito " + (surName));
+
