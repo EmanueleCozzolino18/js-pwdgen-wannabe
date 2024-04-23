148569950707aa7b9dbe91d314e4ebcdf357836d
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-04-23 14:26:49 +0200

    [UPD] Created the variable with log to record the user name

---
+++ js/passgen.js
@@ -0,0 +1,5 @@
+/* STRICT MODE */
+"use strict";
+
+let firstName = prompt("Inserisci il tuo nome:"); /* INSERIMENTO NOME AL INTERNO DELLA VARIABILE FISTNAME */
+console.log("L'utente ha inserito " + (firstName)); /* LOG DEL CONTENUTO DELLA VARIABILE FIRSTNAME */
